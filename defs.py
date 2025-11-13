6b347df8e1b838817962f633de384e1a17408e4b
Ahmed Rashid <AhmedsayidRashid@gmail.com>
2025-11-12 21:58:49 -0500

    feat: create common definitions file for the project
    Signed-off-by: Ahmed Rashid <ahmedsayidrashid@gmail.com>

---
+++ defs.py
@@ -0,0 +1,6 @@
+# This file contains the definitions for the project
+
+# Default settings for the project if not set in the environment variables
+DEFAULT_SEND_SMS_ON_NEW_UNKNOWN = "True"
+DEFAULT_SEND_SMS_ON_RETURNING_UNKNOWN = "False"
+DEFAULT_SMS_COOLDOWN_SECONDS = 300
